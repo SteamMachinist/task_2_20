3de39cae14619511df46ed04886919b9c16bacd9
Yaroslav Roshchupkin <yaroslav-r.01@mail.ru>
2022-03-06 12:08:34 +0300

    implemented algorithm

--- main.py
+++ main.py
@@ -22,12 +22,49 @@ def write_matrix_to_file(filepath, m: list, is_ordered: bool):
     file.close()
 
 
-def is_matrix_ordered_sequence(m: list):
+def is_element_ordered(m: list, row_n: int, column_n: int, seq_order: int, to_right: bool):
+    if row_n == len(m) - 1 and column_n == len(m[row_n]) - 1:
+        return True
+    if to_right:
+        if column_n == len(m[row_n]) - 1:
+            if (m[row_n + 1][column_n] - m[row_n][column_n]) * seq_order < 0:
+                return False
+        else:
+            if (m[row_n][column_n + 1] - m[row_n][column_n]) * seq_order < 0:
+                return False
+    else:
+        if column_n == 0:
+            if (m[row_n + 1][column_n] - m[row_n][column_n]) * seq_order < 0:
+                return False
+        else:
+            if (m[row_n][column_n - 1] - m[row_n][column_n]) * seq_order < 0:
+                return False
+    return True
+
+
+def is_row_ordered(m: list, row_n: int, seq_order: int, to_right: bool, row_range: range):
+    for column_n in row_range:
+        if not is_element_ordered(m, row_n, column_n, seq_order, to_right):
+            return False
+    return True
+
+
+def is_matrix_ordered(m: list):
+    seq_order = (m[0][1] - m[0][0]) / abs(m[0][1] - m[0][0])
+    to_right = True
+    for row_n in range(len(m)):
+        if to_right:
+            if not is_row_ordered(m, row_n, seq_order, to_right, range(len(m[row_n]))):
+                return False
+        else:
+            if not is_row_ordered(m, row_n, seq_order, to_right, range(len(m[row_n]) - 1, -1, -1)):
+                return False
+        to_right = not to_right
     return True
 
 
 if __name__ == '__main__':
-    matrix = read_matrix_from_file("input/input1.txt")
-    write_matrix_to_file("output/output1.txt",
+    matrix = read_matrix_from_file("input/input2.txt")
+    write_matrix_to_file("output/output2.txt",
                          matrix,
-                         is_matrix_ordered_sequence(matrix))
+                         is_matrix_ordered(matrix))
